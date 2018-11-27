33973f2994c83574e8515f41a84040200d17da8f
Luis Alexandre Deschamps Brandão <luis.deschamps.ext@sascar.com.br>
2018-11-27 13:22:06 -0200

    Versão Final para mecanica/hands com rails 4.2

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM luisos:latest
 MAINTAINER Luis Alexandre Deschamps Brandão
 
-RUN yum install -y libyaml-devel mysql-devel
+RUN yum install -y libyaml-devel mysql-devel nodejs
 
 ADD ./ruby.sh /root/ruby.sh
 ADD HandsDeps /root/HandsDeps
 ADD MecanicaDeps /root/MecanicaDeps
 RUN bash /root/ruby.sh
 
-ADD rewriteID.sh /root/rewriteID.sh
+#ADD rewriteID.sh /root/rewriteID.sh
 #RUN bash /root/rewriteID.sh luisos 1001 && \
 #    rm -f /root/rewriteID.sh
 
--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --privileged -d --name luisos-rails  -v /home/luis.deschamps.ext/git/luis/Mecanica:/home/luisos/mecanica -v /home/luis.deschamps.ext/git/luis/HandsHomecare:/home/luisos/handsHomecare luisos-rails
+PROJECTS='/home/luis.deschamps.ext/git/luis'
+
+docker run --privileged -d \
+           --name luisos-rails \
+           -v ${PROJECTS}/Mecanica:/home/luisos/mecanica \
+           -v ${PROJECTS}/HandsHomecare:/home/luisos/handsHomecare \
+           luisos-rails
